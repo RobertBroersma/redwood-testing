3a9ea32ab8e0180d5552459d71723331c8648d3c
Robert Broersma <robertbroersma@gmail.com>
2020-06-21 09:10:09 +0200

    e2e tests

--- web/src/components/BlogPostCell/BlogPostCell.js
+++ web/src/components/BlogPostCell/BlogPostCell.js
@@ -10,8 +10,6 @@ export const QUERY = gql`
 
 export const Loading = () => <div>Loading...</div>
 
-export const Empty = () => <div>Empty</div>
-
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
 export const Success = ({ post }) => {
--- web/src/pages/BlogPostPage/BlogPostPage.test.js
+++ web/src/pages/BlogPostPage/BlogPostPage.test.js
@@ -1,6 +1,4 @@
-import { render, screen, graphql } from '@redwoodjs/testing'
-
-import { db } from '../../../../api/dist/lib/db'
+import { render, screen, graphql, db } from '@redwoodjs/testing'
 
 import BlogPostPage from './BlogPostPage'
 
@@ -15,6 +13,10 @@ describe('BlogPostPage', () => {
 
     render(<BlogPostPage id={newPost.id} />)
 
+    // Tests Loading state
+    expect(screen.getByText(/Loading/i)).toBeInTheDocument()
+
+    // Tests Success state
     expect(await screen.findByText(/Test post/)).toBeInTheDocument()
   })
 
@@ -31,6 +33,7 @@ describe('BlogPostPage', () => {
 
     render(<BlogPostPage id={1} />)
 
+    // Tests Error state
     expect(await screen.findByText(/Whoops!/)).toBeInTheDocument()
   })
 })
